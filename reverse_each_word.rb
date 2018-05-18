615cebb3455f61056a5f28323bf1f7ceb09b41c9
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-18 03:17:14 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,4 @@
 def reverse_each_word(string)
   string_array = string.split
-  string_array.collect do { |word|
-    word.reverse
-  end
+  return string_array.collect do { |word| word.reverse }.join(" ")
 end
