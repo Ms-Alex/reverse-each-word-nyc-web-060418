9d66929252577629f12202608b87566a9c9fce21
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-18 03:16:06 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,6 @@
 def reverse_each_word(string)
-  new_array = []
   string_array = string.split
-  string_array.each do |word|
-    new_array.push(word.reverse)
+  string_array.collect do |word|
+    word.reverse
   end
-  new_array.join(" ")
 end
